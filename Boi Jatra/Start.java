82364f5e40b737623b7c7fb31bbd236eaa25d6e3
ABTAHI ISLAM <23-50434-1@student.aiub.edu>
2025-03-09 16:51:04 +0600

    First Commit

---
+++ Boi Jatra/Start.java
@@ -0,0 +1,49 @@
+import Source.*;
+import FileIo.*;
+import javax.swing.*;
+
+public class Start{
+	public static void main(String [] args){
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				new LogIn();
+				//new Form6("Dip");
+				//new SignUp();
+				//new Form1("Dip");
+				//new Form2("Dip");
+				/*new Form3("Dip");
+				new Form4("Dip");
+				new Form5("Dip");
+				new Form6("Dip");
+				new Form7("Dip");
+				new Form8("Dip");
+				new Form9("Dip");
+				new Form10("Dip");
+				new Form11("Dip");
+				new Form12("Dip");
+				new Form13("Dip");
+				new Form14("Dip");*/
+				//new CartData("Dip","new Book","3","300");
+				//new Cart("Dip");
+				//new Form4("Dip");
+				//new Form5("Dip");
+				//new Form6("Dip");
+				//new Form7("Dip");
+				//new Form8("Dip");
+				//new LogIn();
+				//new Registration();
+				//new AdminRegistration();
+				//new SignUp();
+				//new AdminPanel("Dip");
+				//new UserInfo("Dip");
+				//new OrderInfo("Dip");
+				//new AdminLogIn();
+				//new AddUser("admin");
+				//new AddAdmin("admin");
+				//new Payment1("Dip");
+				//new Bkash("Dip");
+				//new Bank("Dip");
+			}
+		});
+	}
+}
